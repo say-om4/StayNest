6df851afee52f1a54933f75be8b03cc70fba910e
say-om4 <omjeexig@gmail.com>
2026-07-13 01:41:32 +0530

    Connected frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost/PG-Life/backend/api",
+  baseURL: "https://saypglife.free.nf/backend/api",
   headers: {
     "Content-Type": "application/json",
   },
